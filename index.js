ac8214cc7ae1d49c08745f0130bf273101d8ad75
bg560581 <bennett.galgano@gmail.com>
2022-11-28 17:15:46 -0500

    callback practice to make green character move

--- index.js
+++ index.js
@@ -3,6 +3,39 @@ move(inventory).to(0, 0)
 
 const character = newImage('assets/green-character/static.gif')
 
+let direction = null;
+let x = 100;
+let y = 250;
+move(character).withArrowKeys(100,250)
+
+setInterval(function(){
+    if(direction === 'west'){
+        x = x - 1
+    }
+
+    if(direction === 'north'){
+        y = y + 1
+    }
+
+    if(direction === 'south'){
+        y = y - 1
+    }
+
+    if(direction === 'north'){
+        y = y + 1
+    }
+
+    character.style.left = x + 'px'
+    character.style.bottom = y + 'px'
+
+}, 1)
+
+function moveCharacter(){
+    if(direction = null){}
+}
+
+
+
 function handleDirectionChange(direction){
 
     if(direction === null){
